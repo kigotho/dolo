59559219787d3837a30a295b93e60f3d541470eb
kigotho <mkigotho2@gmail.com>
2017-09-26 10:11:04 +0000

    created basic files and included them in index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!-- getting started with Dolo-->
+
+<html lang="en-us">
+
+    <head>
+        <meta charset="utf-8">
+        <title>DOLO</title>
+        <style type="text/css"></style>
+    </head>
+
+    <body>
+        <?php include 'header.php';?>
+        <div id="mainContent">
+	        <?php include 'section.php';?>
+        </div>
+        <?php include 'article.php';?>
+        <?php include 'footer.php';?>
+        <script> 
+
+        </script>
+
+    </body>
+</html>
